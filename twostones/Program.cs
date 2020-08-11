352430857c10b79f5462f3b539c1c47db4f64ba1
Josefin H <josefin.hallsson@gmail.com>
2020-08-11 13:43:41 +0200

    Add project files.

---
+++ twostones/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace twostones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stones = int.Parse(Console.ReadLine());
+            var stonesLeft = stones;
+
+            for (int i = 1; i <= stones-1; i++)
+            {
+                if(stonesLeft>1)
+                stonesLeft -= 2;
+            }
+            var winner = "";
+            if (stonesLeft % 2 == 0)
+                winner = "Bob";
+            else
+                winner = "Alice";
+            Console.WriteLine(winner);
+        }
+    }
+}
